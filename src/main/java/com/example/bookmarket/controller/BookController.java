dffa3385a35b42543d95a55c42828fca95c7cf26
Gi0000 <102645896+Gi0000@users.noreply.github.com>
2025-05-12 16:11:23 +0900

    4. 컨트롤러 구현

--- src/main/java/com/example/bookmarket/controller/BookController.java
+++ src/main/java/com/example/bookmarket/controller/BookController.java
@@ -5,20 +5,34 @@ import com.example.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/books")
 public class BookController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    @GetMapping
     public String requestBookLsit(Model model) {
         List<Book> list = bookService.getAllBookList();
         model.addAttribute("bookList", list);
         return "books";
     }
+
+    @GetMapping("/all")
+    public ModelAndView requestAllBooks() {
+        ModelAndView modelAndView = new ModelAndView();
+        List<Book> list = bookService.getAllBookList();
+        modelAndView.addObject("bookList", list);
+        modelAndView.setViewName("books");
+        return modelAndView;
+    }
+
+
 }
